23aed3615d4b3e631a41bc474ee1ec92cdcbb896
mifnaufal <naufalhhdd@gmail.com>
2025-08-13 08:57:26 +0700

    hahah

--- uji_doank/lib/main.dart
+++ uji_doank/lib/main.dart
@@ -3,6 +3,8 @@ import 'bab4/onChanged.dart' as onChangedExample;
 import 'bab4/GestureDetector.dart' as gestureExample;
 import 'bab4/InputdariPengguna.dart' as userInputExample;
 import 'bab4/MengelolaStateLokal.dart' as stateExample;
+import 'bab3/latihan/bab3.dart' as bab3Example;
+import 'bab4/latihan/bab4.dart' as bab4Example;
 
 void main() {
   runApp(const MainApp());
@@ -89,6 +91,30 @@ class HomeScreen extends StatelessWidget {
                 );
               },
               child: const Text('State Management Example'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const bab3Example.MyProfileCard(),
+                  ),
+                );
+              },
+              child: const Text('Bab 3 Profile Card'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const bab4Example.SimpleCalculatorApp(),
+                  ),
+                );
+              },
+              child: const Text('Bab 4 Calculator'),
             ),
           ],
         ),
